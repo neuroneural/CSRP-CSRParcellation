cbbd6935ba4a52323c71848ad5c4002ed1dd20a3
William Ashbee <wsashbee@gmail.com>
2024-03-24 21:50:36 +0000

    monai pytorch 1.12 merge seems to be working, need to test segmentation.

--- singularity/train/trainsurflhgm.sh
+++ singularity/train/trainsurflhgm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-. /opt/miniconda3/bin/activate cortexode
+#. /opt/miniconda3/bin/activate cortexode
+. /opt/miniconda3/bin/activate csrf
 cd /cortexode
 
 # model_gm_adni_lh_exp6_300epochs.pt
@@ -8,4 +9,5 @@ cd /cortexode
 # model_wm_adni_rh_exp6_270epochs.pt
 #--model_type="notcsrf"
 #csrf-data-dev
-python train.py --train_type='surf' --data_dir='/speedrun/cortexode-data-rp/' --model_dir='/cortexode/ckpts/exp_csrf_v2/model/' --init_dir='/cortexode/ckpts/exp_csrf_v2/init/' --data_name='adni'  --surf_hemi='lh' --surf_type='gm' --n_epochs=401 --n_samples=150000 --tag='csrf' --solver='rk4' --step_size=0.1 --device='gpu' 
+#python train.py --train_type='surf' --data_dir='/speedrun/cortexode-data-rp/' --model_dir='/cortexode/ckpts/exp_csrf_v2/model/' --init_dir='/cortexode/ckpts/exp_csrf_v2/init/' --data_name='adni'  --surf_hemi='lh' --surf_type='gm' --n_epochs=401 --n_samples=150000 --tag='csrf' --solver='rk4' --step_size=0.1 --device='gpu' 
+python train.py --train_type='surf' --data_dir='/speedrun/csrf-data-dev/' --model_dir='/cortexode/ckpts/exp_csrf_v3/model/' --init_dir='/cortexode/ckpts/exp_csrf_v3/init/' --data_name='adni'  --surf_hemi='lh' --surf_type='gm' --n_epochs=100 --n_samples=150000 --tag='csrf' --solver='rk4' --step_size=0.1 --device='gpu' 
