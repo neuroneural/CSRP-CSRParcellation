bbdda2dbbf886e6399eebb7eabe074790e21226f
William Ashbee <wsashbee@gmail.com>
2024-11-15 16:08:40 +0000

    hcp init

--- hcpanalysis/singularity/hcp/init/initsurflh.sh
+++ hcpanalysis/singularity/hcp/init/initsurflh.sh
@@ -2,8 +2,8 @@
 . /opt/miniconda3/bin/activate csrf
 cd /cortexode
 
-# python eval.py --test_type='init' --data_dir='/cortexode/bsnipanalysis/' --model_dir='/cortexode/ckpts/bsnip/bsnip_0/model/' --init_dir='/cortexode/ckpts/bsnip/bsnip_0/init/test/' --data_usage='test' --data_name='bsnip' --surf_hemi='lh' --tag='Unet' --device='gpu'
+python eval.py --test_type='init' --data_dir='/speedrun/cortexode-data-rp/' --model_dir='/cortexode/ckpts/bsnip/hcp_0/model/' --init_dir='/cortexode/ckpts/bsnip/hcp_0/init/test/' --data_usage='test' --data_name='adni' --surf_hemi='lh' --tag='Unet' --device='gpu'
 
-# python eval.py --test_type='init' --data_dir='/cortexode/bsnipanalysis/' --model_dir='/cortexode/ckpts/bsnip/bsnip_0/model/' --init_dir='/cortexode/ckpts/bsnip/bsnip_0/init/train/' --data_usage='train' --data_name='bsnip' --surf_hemi='lh' --tag='Unet' --device='gpu'
+python eval.py --test_type='init' --data_dir='/speedrun/cortexode-data-rp/' --model_dir='/cortexode/ckpts/bsnip/hcp_0/model/' --init_dir='/cortexode/ckpts/bsnip/hcp_0/init/train/' --data_usage='train' --data_name='adni' --surf_hemi='lh' --tag='Unet' --device='gpu'
 
-python eval.py --test_type='init' --data_dir='/cortexode/bsnipanalysis/' --model_dir='/cortexode/ckpts/bsnip/bsnip_0/model/' --init_dir='/cortexode/ckpts/bsnip/bsnip_0/init/valid/' --data_usage='valid' --data_name='bsnip' --surf_hemi='lh' --tag='Unet' --device='gpu'
+python eval.py --test_type='init' --data_dir='/speedrun/cortexode-data-rp/' --model_dir='/cortexode/ckpts/bsnip/hcp_0/model/' --init_dir='/cortexode/ckpts/bsnip/hcp_0/init/valid/' --data_usage='valid' --data_name='adni' --surf_hemi='lh' --tag='Unet' --device='gpu'
